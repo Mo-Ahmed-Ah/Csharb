6163d2ecb05765d9ee7375fb201c569a54660d65
Ahmed Mohamed <ahmed7777003330@gmail.com>
2024-09-13 12:09:50 +0300

    add Calcoulator

---
+++ Calcoulator/Calcoulator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace Calcoulator
+{
+    class Calcoulator
+    {
+        static void Main()
+        {
+            char ca;
+            bool check = true;
+            while (check)
+            {
+                Console.Write("What the calcoulatio (+ , - , / or *) ? ");
+                ca =  char.Parse(Console.ReadLine());
+
+                Console.Write("Enter the number one : ");
+                int num_1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter the number two : ");
+                int num_2 = int.Parse(Console.ReadLine());
+                if(ca == '+')
+                {
+                    Console.WriteLine("{0} + {1} = {2}",num_1 , num_2 , num_1 + num_2);
+
+                    Console.Write("whant use again (Y or N) : ");
+                    ca = char.Parse(Console.ReadLine());
+                    if (ca == 'n' || ca == 'N')
+                    {
+                        check = false;
+                    }
+                }
+                else if (ca == '-')
+                {
+                    Console.WriteLine("{0} - {1} = {2}", num_1, num_2, num_1 - num_2);
+                    Console.Write("whant use again (Y or N) : ");
+                    ca = char.Parse(Console.ReadLine());
+                    if (ca == 'n' || ca == 'N')
+                    {
+                        check = false;
+                    }
+                }
+                else if (ca == '*')
+                {
+                    Console.WriteLine("{0} x {1} = {2}", num_1, num_2, num_1 * num_2);
+                    Console.Write("whant use again (Y or N) : ");
+                    ca = char.Parse(Console.ReadLine());
+                    if (ca == 'n' || ca == 'N')
+                    {
+                        check = false;
+                    }
+                }
+                else if (ca == '/')
+                {
+                    if (num_2 != 0) {
+                        Console.WriteLine("{0} / {1} = {2}", num_1, num_2, num_1 / num_2);
+
+                        Console.Write("whant use again (Y or N) : ");
+                        ca = char.Parse(Console.ReadLine());
+                        if (ca == 'n' || ca == 'N')
+                        {
+                            check = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("the oberation is not trou number tow equal 0");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter (+ , - , / or *)");
+                }
+
+                Console.WriteLine("Good By..!");
+
+            }
+        }
+
+    }
+}
